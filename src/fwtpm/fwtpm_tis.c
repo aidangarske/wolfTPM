15fe369960c9011131302685537ea48bb4a17cfc
Aidan Garske <aidan@wolfssl.com>
2026-09-08 12:54:21 -0700

    F-9193 - Assert the TIS FIFO can hold a full command and response

--- src/fwtpm/fwtpm_tis.c
+++ src/fwtpm/fwtpm_tis.c
@@ -51,6 +51,11 @@
  * VID = "WF" (0x4657), DID = "TP" (0x5054) */
 #define FWTPM_TIS_DID_VID_VAL  0x50544657UL
 
+/* The FIFO/response buffer must hold a full command and response; a smaller
+ * FWTPM_TIS_FIFO_SIZE override would let FWTPM_ProcessCommand overflow it. */
+typedef char fwtpm_tis_fifo_size_check_[
+    (FWTPM_TIS_FIFO_SIZE >= FWTPM_MAX_COMMAND_SIZE) ? 1 : -1];
+
 /* --- Static helpers --- */
 
 /* Extract register offset from a full TIS address.
