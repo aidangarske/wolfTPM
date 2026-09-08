575c670c5635ff0e1071885fa79b37ebda03d7ec
Aidan Garske <aidan@wolfssl.com>
2026-09-08 12:01:40 -0700

    F-11932 - Zeroize hierarchy auth buffer in FWTPM_NV_SaveAuth

--- src/fwtpm/fwtpm_nv.c
+++ src/fwtpm/fwtpm_nv.c
@@ -2375,6 +2375,8 @@ int FWTPM_NV_SaveAuth(FWTPM_CTX* ctx, UINT32 hierarchy)
     if (rc == 0) {
         rc = FwNvAppendEntry(ctx, tag, buf, (UINT16)pos);
     }
+
+    TPM2_ForceZero(buf, sizeof(buf));
     return rc;
 }
 
